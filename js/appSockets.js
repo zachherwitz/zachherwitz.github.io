6d39cf4b663d3af046a531256683af8f24c572ae
Zach Herwitz <zach.herwitz@gmail.com>
2020-05-17 00:10:37 -0400

    so far so good readding it bit by bit

--- js/appSockets.js
+++ js/appSockets.js
@@ -2,6 +2,9 @@ $(() => {
 
   console.log('linked!');
 
+  // Creating Global variables
+  let $userInput = '';
+
 
           // HTML ELEMENTS // HTML ELEMENTS // HTML ELEMENTS // HTML ELEMENTS //
 
@@ -11,11 +14,19 @@ $(() => {
 
   // Creating Form Innards:
   let $input = $('<input>')
-                .attr({'type': 'text', 'id': 'inputField', 'name': 'inputField', 'value': '我'})
+                .attr({'type': 'text', 'id': 'inputField', 'name': 'inputField', 'placeholder': 'enter char'})
                 .click(
                   (event) => {
                     event.preventDefault()
+                    $(event.currentTarget).val('')
                   })
+                .keydown((event) => {
+                  // https://www.w3schools.com/jsref/event_key_code.asp
+                  if(event.code === 'Enter') {
+                    captureUserInput(); // captures the user input and saves it to $userInput
+                    callData($userInput);
+                  }
+                })
 
 
                   // APPENDING // APPENDING // APPENDING // APPENDING //
@@ -30,9 +41,18 @@ $(() => {
 
                 // DICTIONARY FUNCTIONALITY // DICTIONARY FUNCTIONALITY //
 
-  const callData = (userInput) => {
+
+// Save and reset input field on ENTER
+  const captureUserInput = () => {
+    event.preventDefault(); // preventing page refresh
+    $userInput = $(event.currentTarget).val(); // saving user input to variable
+    $(event.currentTarget).val('') // clearing user input field;
+  }
+
+// Calls on the website to provide information based on userInput
+  const callData = (input) => {
     $.ajax({
-      url: `https://api.ctext.org/getcharacter?char=${}`
+      url: `https://api.ctext.org/getcharacter?char=${input}`
     }).then(
       (data) => {
         console.log(data);
