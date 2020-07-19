e66ff04807b339ff89133522a24ec30d2516d3f0
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-19 05:53:17 -0400

    Updates

---
+++ precache-manifest.08587c3a84bb67f4ac1ad50b6bf55440.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0ee9c47cc196f19cb092f941cc938f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "763685e33c09f2f9048f",
+    "url": "/portfolio/static/css/main.2226d3ea.chunk.css"
+  },
+  {
+    "revision": "8bee5d5100e245860e14",
+    "url": "/portfolio/static/js/2.4f930bc0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.4f930bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763685e33c09f2f9048f",
+    "url": "/portfolio/static/js/main.d18b039e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "cb39ec93d9e7f5da0d4d31a7f1f44498",
+    "url": "/portfolio/static/media/biopic.cb39ec93.jpg"
+  },
+  {
+    "revision": "cb19b3e031700c1aa0e93fca4acc38a2",
+    "url": "/portfolio/static/media/resume.cb19b3e0.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca6bb8b0080913e6f74d8e2b6abbccea.js"
+  "/portfolio/precache-manifest.08587c3a84bb67f4ac1ad50b6bf55440.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
